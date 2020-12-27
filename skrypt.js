6a09dbd754c3a73d3204f7f17dadca7f3e92864b
DudekKonrad <kdudek111@wp.pl>
2020-12-27 12:49:46 +0100

    Correct expanding device settings

--- skrypt.js
+++ skrypt.js
@@ -1,3 +1,11 @@
 function openTab(tabName) {
 	document.getElementById(tabName).style.display = "block";
-  }
+	document.getElementById(tabName).style.width = "500px";
+	document.getElementById('device_more').style.display = "none";
+}
+
+function closeTab(tabName) {
+	document.getElementById(tabName).style.display = "none";
+	document.getElementById(tabName).style.width = "500px";
+	document.getElementById('device_more').style.display = "block";
+}
