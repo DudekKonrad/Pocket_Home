4595ebe8ff1dca2a486fe8843df75c544cde67d2
DudekKonrad <kdudek111@wp.pl>
2020-12-27 21:00:17 +0100

    Proper device settings expand for 1 device

--- skrypt.js
+++ skrypt.js
@@ -1,11 +1,14 @@
 function openTab(tabName) {
 	document.getElementById(tabName).style.display = "block";
-	document.getElementById(tabName).style.width = "500px";
-	document.getElementById('device_more').style.display = "none";
+	var p_width = document.getElementById("devices").offsetWidth;
+	document.getElementById(tabName).style.width += p_width / 2 + "px";
+	var p_height = document.getElementById("devices").offsetHeight;
+	document.getElementById(tabName).style.height += p_height / 2 + "px";
+	document.getElementById('more_button').style.display = "none";
+	document.getElementById('more_button').style.maxWidth = 'none';
 }
 
 function closeTab(tabName) {
 	document.getElementById(tabName).style.display = "none";
-	document.getElementById(tabName).style.width = "500px";
-	document.getElementById('device_more').style.display = "block";
+	document.getElementById('more_button').style.display = "block";
 }
